c0365b9530eff20a0231afb53a712092e35bf5b6
gitqwerty777 <goohcl777@gmail.com>
2015-05-29 22:41:41 +0800

    modify type.py

--- type.py
+++ type.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 _InitCardsPerPlayer_ = 5
 _TotalPlayerNum_ = 4
 _IamDead_ = -1
@@ -8,8 +9,8 @@ _MaxComb_ = 32
 
 import random
 import time
-import action
-import ab_agent
+from action import Action
+from ab_agent import ScoutAgent
 
 class PossibleCombination:
     def __init__(self, comb = list()):
@@ -27,7 +28,17 @@ class State:
 
 class Judge:
     def __init__(self, h = list(), c = [[0 for x in range(5)] for x in range(4)], m=list(), p=0, cw=1, cp=1):
-        #self.player_state = ps #what is this
+        players = list()
+        players.append(ScoutAgent())
+        #fake action
+        self._possibleActions_ = list()
+        self._possibleActions_.append(Action())
+        self._possibleActions_.append(Action())
+        self._possibleActions_.append(Action())
+        self._possibleActions_.append(Action())
+        random.shuffle(players)
+        self.player = players
+
         self.history = h #action list
         self.card = c # need to sort by cardvalue,two dimension list
         self.mountain = m
@@ -40,34 +51,21 @@ class Judge:
         self._possibleActions_ = list()
         self.initBoard()
         self.rand4Cards()
-        self.player = ab_agent.ScoutAgent()
-        self._possibleActions_.append(action.Action())
-        self._possibleActions_.append(action.Action())
-        self._possibleActions_.append(action.Action())
-        self._possibleActions_.append(action.Action())
-        state = ab_agent.PlayerState(self.history, self._possibleActions_, self.card[self.current_player], len(self.card[0]), len(self.card[1]), len(self.card[2]), len(self.card[3]), len(self.mountain), self.point, self.clock_wise)
-        self.player.genmove(state)
-
+        self.printBoard()
+        av = self.getAction()
+        print av
         
-        '''while(not self.isGameFinished()):
-        this.writeFile()
-            for i in range(_MaxComb_ * _MaxActionLength_):
-                parsingBuf_possibleActions[i] = '\0';
-            for i in range(_possibleActions_.size()):
-                sprintf(ourBuf, "%d %d %d %d %d %d %d;", _possibleActions_[i].user, _possibleActions_[i].cards_used[0], _possibleActions_[i].cards_used[1], _possibleActions_[i].cards_used[2], _possibleActions_[i].cards_used[3], _possibleActions_[i].cards_used[4], _possibleActions_[i].victim);
-                strncat(parsingBuf_possibleActions, ourBuf, strlen(ourBuf));
+        while not self.isGameFinished():
+            state = ab_agent.PlayerState(self.history, self._possibleActions_, self.card[self.current_player], len(self.card[0]), len(self.card[1]), len(self.card[2]), len(self.card[3]), len(self.mountain), self.point, self.clock_wise)
+            a = self.player[self.current_player].genmove(state)
+            self.doAction(a)
 
-    if((pid = fork()) == 0) //  child
-    {
-      //  TODO: transmit history, _possibleActions_
-      execlp(_PlayerExecName_, _PlayerExecName_, parsingBuf_possibleActions ,(char*)0);
-    }
+        winner = 0
+        for i in range(4):
+            if len(self.card[i]) > 0:
+                winner = i
+        print "winner is " + i
 
-    //  TODO: wait for your child to be dead...
-    waitpid(-1, &status, 0);'''
-
-        a = self.readFile()
-        self.doAction(a)
     def rand4Cards(self):
         original_cards = list()
         random.seed(time.time())
@@ -77,37 +75,36 @@ class Judge:
             for counter in range(_InitCardsPerPlayer_):
                 pick = random.randint(0,1024) % len(original_cards)
                 self.card[i][counter] = original_cards[pick]
-                swap(original_cards[pick], original_cards[len(original_cards) - 1])
-                original_cards.pop()
+                original_cards.pop(pick)
         #//	TODO:	set mountain
         for i in range(len(original_cards)):
             pick = random.randint(0, 1024) % len(original_cards);
             self.mountain.append(original_cards[pick])
-            swap(original_cards[pick], original_cards[len(original_cards) - 1])
-            original_cards.pop()
+            original_cards.pop(pick)
     
     def initBoard(self):
         self.current_player = 1
         self.clock_wise = 1 #1 and -1
-    #cards
+        self.playerNum = 4
+        self.isDead = [False]*self.playerNum
 
     def isGameFinished(self):
-        pass
-    #return this.card[0].length() == 0...;
+        deadcount = 0
+        for i in range(self.playerNum):
+            if self.isDead[i]:
+                deadcount += 1
+        if deadcount == self.playerNum -1:
+            print(gamefinished)
+            return True
+        return False
 
     def printBoard(self):
-        pass
+        print "mountnum = " + str(len(self.mountain)) + ".Now %dth Move. Player %d" % (len(self.history), self.current_player) + "\n" \
+            + "North(id = 1):" + (getCardsString(self.card[0])) \
+            + "East(id = 2):" + (getCardsString(self.card[1])) \
+            + "South(id = 3):" + (getCardsString(self.card[2])) \
+            + "West(id = 4):" + (getCardsString(self.card[3])) 
     
-    def writeFile(self):
-        pass
-    #generateStateData();
-
-    def generateStateData(self):
-        _possibleActions_ = self.getAction()
-
-    def readFile(self):
-        pass
-
     def doAction(self, a):
         self.history.append(a)
         self.current_player += self.clock_wise
@@ -117,18 +114,16 @@ class Judge:
             self.current_player = 1
         self.current_player += self.clock_wise
 
-    def TellAgent(self, which_agent, what_happened):
-        pass
         
     def getAction(self): # get legal action list
-        card = self.card[current_player];
+        card = self.card[self.current_player];
         isuse = [False]*len(card) # size = card
         av = list()
         a = Action(self.current_player)
-        while(nextbool(av, n)):
+        while nextbool(av, len(card)):
             nowv = 0
             a.cards = []
-            for i in range(n):
+            for i in range(len(card)):
                 if isuse[i]:
                     nowv += card[i]%13
                     a.cards.append(card[i])
@@ -136,17 +131,16 @@ class Judge:
             if nowv > 13:
                 continue
             else:
-                nowv %= 13 #?
-            
+                nowv %= 13 
         if nowv == 7 or nowv == 9:
             for i in range(_TotalPlayerNum_):
-                if i == self.current_player or self.card[i].length() == 0: #todo: isdead?
+                if i == self.current_player or self.isDead[i]:
                     continue
                 a.victim = i
                 av.append(a)
         elif nowv == 5:
             for i in range(_TotalPlayerNum_):
-                if self.card[i].length() == 0: #todo: isdead?
+                if self.isDead[i] == 0:
                     continue
                 a.victim = i
                 av.append(a)
@@ -167,50 +161,56 @@ class Judge:
         for i in range(_MaxCombCardNum_):
             cardValue += a.cards_used[i]
             if(a.cards_used[i] == 1):#space one
-                iszero = true
+                iszero = True
         if iszero and cardValue == 1:
-            return true
+            return True
 
         cardValue = cardValue % 13
         if cardValue == 7:
             if a.victim > 0 and a.victim <= 4 and a.victim != a.user and len(card[a.victim-1]) >= 1:#//now user id?
-                return true
+                return True
         elif cardValue == 9:
             if a.victim > 0 and a.victim <= 4 and a.victim != a.user:#//now user id?
-                return true
+                return True
         elif cardValue == 5:
             if a.victim > 0 and a.victim <= 4:
-                return true
+                return True
         elif cardValue == 12 or cardValue == 10:
             value = 20 if (cardValue % 13 == 12) else 10
             if a.victim == -1 and self.point + value <= 99 or a.victim == -2 and self.point - value >= 0:
-                return true
+                return True
         elif cardValue == 4 or cardValue == 11 or cardValue == 13:
-            return true
+            return True
         else:
             if self.point+cardValue <= 99:
-                return true
-        return false
+                return True
+        return False
 
 def nextbool(vb, n):
     nowv = 0
+    n = len(vb)
     for i in range(n):
         nowv *= 2
         nowv += 1 if (vb[i]) else 0
     nowv = nowv +1
-    print nowv
+    print "nowv in nextbool:" + str(nowv)
     if nowv >= power(2, n):
-        return False, vb
+        return False
     for i in range(n, 0, -1):
         vb[i] = True if (nowv%2) else False
         nowv /= 2
     print vb
-    return True, vb
+    return True
 
-def swap(a, b):
-    c = a
-    a = b
-    b = c
+cardType = ['♠ ', '♥ ', '♦ ', '♣ ']
+def getCardsString(l):
+    s = ""
+    for card in l:
+        s += getCardString(card) + ", "
+    s += "\n"
+    return s
+def getCardString(cardIndex):
+    return str(cardIndex % 13 + 1) + cardType[(cardIndex-1)/13]
 
 if __name__ == "__main__" :
     j = Judge()
